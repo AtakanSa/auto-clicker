1d231d6284660b29b9d27ee7356bb395cf69aa70
Atakan Sari <atakannsari@gmail.com>
2024-10-25 11:45:14 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,37 @@
+import pyautogui
+import time
+
+print("Fare koordinatlarınızı görmek için 5 saniye bekleyin...")
+time.sleep(5)
+
+x, y = pyautogui.position()  # Mevcut fare pozisyonunu al
+print(f"Koordinatlar: X={x}, Y={y}")
+
+print("Fare koordinatlarınızı görmek için 5 saniye bekleyin...")
+time.sleep(5)
+
+x, y = pyautogui.position()  # Mevcut fare pozisyonunu al
+print(f"Koordinatlar: X={x}, Y={y}")
+
+print("Fare koordinatlarınızı görmek için 5 saniye bekleyin...")
+time.sleep(5)
+
+x, y = pyautogui.position()  # Mevcut fare pozisyonunu al
+print(f"Koordinatlar: X={x}, Y={y}")
+
+print("Fare koordinatlarınızı görmek için 5 saniye bekleyin...")
+time.sleep(5)
+
+x, y = pyautogui.position()  # Mevcut fare pozisyonunu al
+print(f"Koordinatlar: X={x}, Y={y}")
+print("Fare koordinatlarınızı görmek için 5 saniye bekleyin...")
+time.sleep(5)
+
+x, y = pyautogui.position()  # Mevcut fare pozisyonunu al
+print(f"Koordinatlar: X={x}, Y={y}")
+
+print("Fare koordinatlarınızı görmek için 5 saniye bekleyin...")
+time.sleep(5)
+
+x, y = pyautogui.position()  # Mevcut fare pozisyonunu al
+print(f"Koordinatlar: X={x}, Y={y}")
